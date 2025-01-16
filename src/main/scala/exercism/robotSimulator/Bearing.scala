6f9ea22120992fe8c6b6f37c1fddb05e169c08d7
Roshan Gautam <106965552+Roshangautam2052@users.noreply.github.com>
2025-01-16 14:41:07 +0000

    Robot simulator (#8)
    * type erasure error
    
    * Complete RobotSimulator

--- src/main/scala/exercism/robotSimulator/Bearing.scala
+++ src/main/scala/exercism/robotSimulator/Bearing.scala
@@ -1,8 +1,11 @@
 package exercism.robotSimulator
 
+sealed abstract class Bearing (name:String)
+
 object Bearing {
-  val North: String = "North"
-  val South: String = "South"
-  val East: String = "East"
-  val West: String = "West"
+  case object North extends Bearing("North")
+  case object South extends Bearing("South")
+  case object East extends Bearing("East")
+  case object West extends Bearing("West")
 }
+
---
+++ src/main/scala/exercism/robotSimulator/Robot.scala
@@ -0,0 +1,4 @@
+package exercism.robotSimulator
+
+case class Robot(val bearing: Bearing, val position:(Int, Int)) {
+}
